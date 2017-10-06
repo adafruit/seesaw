a8584722f4bed08a987168ccd57c34acf62583a7
dean <deanm1278@gmail.com>
2017-10-06 11:51:27 -0400

    DM: add config, remove board-specific build

--- seesaw/joyFeather/build_date.h
+++
@@ -1,6 +0,0 @@
-#ifndef BUILD_DATE_H
-#define BUILD_DATE_H
-#define __DAY__ 29
-#define __MONTH__ 9
-#define __YEAR__ 017
-#endif
--- seesaw/joyFeather/qp/ports/arm-cm/qxk/gnu/qxk_port.d
+++
@@ -1,2 +0,0 @@
-qp/ports/arm-cm/qxk/gnu/qxk_port.d qp/ports/arm-cm/qxk/gnu/qxk_port.o: \
- ../qp/ports/arm-cm/qxk/gnu/qxk_port.S
