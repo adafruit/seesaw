be6af99818ae356fd6a82cbd8410a399f14d4906
wallarug <wallarug@coliemore.com.au>
2019-06-27 22:30:36 +1000

    bsp_adc.c

--- bsp/bsp_adc.h
+++ bsp/bsp_adc.h
@@ -6,8 +6,22 @@
 void adc_init();
 void adc_set_freerunning(bool mode);
 
-inline void syncADC() { while (ADC->STATUS.bit.SYNCBUSY == 1); }
-inline void adc_trigger(){ syncADC(); ADC->SWTRIG.bit.START = 1;}
+inline void syncADC() { 
+	#if defined(SAMD51)
+	while(ADC0->SYNCBUSY.reg & ADC_SYNCBUSY_CTRLB); //wait for sync
+	while(ADC1->SYNCBUSY.reg & ADC_SYNCBUSY_CTRLB); //wait for sync
+	#else
+	while (ADC->STATUS.bit.SYNCBUSY == 1); 
+	#endif
+	}
+inline void adc_trigger(){ syncADC(); 
+	#if defined(SAMD51)
+	ADC0->SWTRIG.bit.START = 1;
+	ADC1->SWTRIG.bit.START = 1;
+	#else
+	ADC->SWTRIG.bit.START = 1;
+	#endif
+	}
 
 void adc_set_inputscan(uint8_t channels);
 
